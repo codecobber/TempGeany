f9c4e9cac96b34ed2df74c733d3ebeaa28216e49
Cobber <codecobber@gmail.com>
2022-04-19 14:42:48 +0100

    Create rowData.js
    rowData.js added

---
+++ js/rowData.js
@@ -0,0 +1,215 @@
+var rowData = [
+    {
+    "rowNo":"01",
+    "prop":"row"
+    },
+    {
+    "rowNo":"02",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"03",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"04",
+    "prop":"row"
+    },
+    {
+    "rowNo":"05",
+    "prop":"row"
+    },
+    {
+    "rowNo":"06",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"07",
+    "prop":"row"
+    },
+    {
+    "rowNo":"08",
+    "prop":"row"
+    },
+    {
+    "rowNo":"09",
+    "prop":"row"
+    },
+    {
+    "rowNo":"10",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"11",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"12",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"13",
+    "prop":"row"
+    },
+    {
+    "rowNo":"14",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"15",
+    "prop":"row"
+    },
+    {
+    "rowNo":"16",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"17",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"18",
+    "prop":"row"
+    },
+    {
+    "rowNo":"19",
+    "prop":"row"
+    },
+    {
+    "rowNo":"20",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"21",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"22",
+    "prop":"row"
+    },
+    {
+    "rowNo":"23",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"24",
+    "prop":"row"
+    },
+    {
+    "rowNo":"25",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"26",
+    "prop":"row"
+    },
+    {
+    "rowNo":"27",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"28",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"29",
+    "prop":"row"
+    },
+    {
+    "rowNo":"30",
+    "prop":"row"
+    },
+    {
+    "rowNo":"31",
+    "prop":"row"
+    },
+    {
+     "rowNo":"32",
+     "prop":"row text-left"
+    },
+    {
+    "rowNo":"33",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"34",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"35",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"36",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"37",
+    "prop":"row"
+    },
+    {
+    "rowNo":"38",
+    "prop":"row"
+    },
+    {
+    "rowNo":"39",
+     "prop":"row"
+    },
+    {
+    "rowNo":"40",
+    "prop":"row"
+    }
+    ,
+    {
+    "rowNo":"41",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"42",
+    "prop":"row"
+    },
+    {
+    "rowNo":"43",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"44",
+    "prop":"row"
+    },
+    {
+    "rowNo":"45",
+    "prop":"row"
+    },
+    {
+    "rowNo":"46",
+    "prop":"row"
+    },
+    {
+    "rowNo":"47",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"48",
+    "prop":"row text-left"
+    },
+    {
+    "rowNo":"49",
+    "prop":"row"
+    },
+    {
+    "rowNo":"50",
+    "prop":"row"
+    },
+    {
+    "rowNo":"51",
+    "prop":"wide"
+    },
+    {
+    "rowNo":"52",
+    "prop":"wide"
+    },
+    {
+    "rowNo":"53",
+    "prop":"row"
+    }
+];
